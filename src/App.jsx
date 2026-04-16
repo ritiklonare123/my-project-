186d009364c8fc42557bd7c51e2c84336c658bc6
ritiklonare123 <ritiklonare5@gmail.com>
2026-04-16 13:18:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+// import React, { useState } from 'react'
+
+// const App = () => {
+//   const [title, setTitle] = useState('');
+//   const [arr, setarr] = useState([]); 
+
+//    const DelteTask =(idx)=>{
+//        console.log(idx)
+//    }
+ 
+// localStorage.setItem("user",{name : "ritik",age : 23});
+
+
+//   return (
+//     <div>
+//       <form onSubmit={(e)=>{
+//            e.preventDefault();
+//            let newArr= [...arr,title];
+//              setarr(newArr)
+//            console.log(arr)
+//            setTitle('');
+//       }} >
+//         <input type="text" placeholder='Enter your name' value={title}  onChange={(e)=>setTitle(e.target.value)} />
+//         <button>Submit</button>
+//       </form>
+
+//       {arr.map((ele,key)=>{
+//         return <div key={key} >
+//           <h1 >title is {ele} </h1>
+//             <button onClick={()=>{DelteTask(key)}} >delete</button>
+//         </div> 
+//       })}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+import {React ,useEffect, useState} from "react"
+
+const App = () => {
+  const [count, setcount] = useState(0)
+  useEffect(()=>{
+    console.log("useeffict is calling ")
+  },[])
+  useEffect(()=>{
+    console.log("second useeffect is calling ")
+  },[])
+  return (
+    <div>
+          <h2>{count}</h2>
+      <button onClick={()=>setcount(count+1)}>click me !</button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/comp/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = ({obj}) => {
+  return (
+    <>
+    <h2  > my name is {obj.name} and my age is {obj.age}</h2>
+    </>
+  )
+}
+
+export default Card
